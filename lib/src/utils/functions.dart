80f5f52d9e1064795a444924824be52a0f9ab98e
Nayef Radwi <naif.mazen.radwi@gmail.com>
2024-02-15 20:50:07 +0300

    added some common functions that were useful

--- lib/src/utils/functions.dart
+++ lib/src/utils/functions.dart
@@ -1,1 +1,16 @@
+extension FilterNullMap<T, P> on Map<T, P> {
+  Map<T, P> filterOutNullValues() {
+    final output = <T, P>{};
+    for (final entry in entries) {
+      if (entry.value == null) continue;
+      output[entry.key] = entry.value;
+    }
+    return output;
+  }
+}
 
+extension FilterNullList<T> on List<T> {
+  List<T> filterOutNullValues() {
+    return where((element) => element != null).toList();
+  }
+}
